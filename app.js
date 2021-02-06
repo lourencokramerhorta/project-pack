a3bc293dcb2d87f748600fa43544ca1df0755014
catarinabombaca <42444764+catarinabombaca@users.noreply.github.com>
2021-02-06 12:05:18 +0000

    Parks list
    - Created park view fle
    - Created park.routes.js file
    - Added GET route for /home that will display lists of parks

--- app.js
+++ app.js
@@ -45,11 +45,13 @@ app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 app.locals.title = "Express - Generated with IronGenerator";
 
 //Get routes
+const parkRouter = require('./routes/park.routes')
 const dogRouter = require('./routes/dog.routes');
 const authRouter = require("./routes/auth.routes");
 const index = require("./routes/index");
 app.use("/", index);
 app.use("/", authRouter);
 app.use('/', dogRouter);
+app.use('/', parkRouter);
 
 module.exports = app;
