ecdaab9c047643e770018d522eedbd12ae51ea91
catarinabombaca <42444764+catarinabombaca@users.noreply.github.com>
2021-02-12 23:15:02 +0000

    Show DB parks in map

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,5 @@
-  let defaultLocation = { lat: 0, lng: 0 };
-
+//Get default location  
+let defaultLocation = { lat: 38.71667, lng: -9.13333 };
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -7,28 +7,52 @@
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
-        console.log(
-          `User position: ${defaultLocation.lat}, ${defaultLocation.lng}`
-        );
       },
       () => {
         console.log("Error in the geolocation service.");
       }
     );
-  } else {
-    defaultLocation = console.log(`Default position: ${defaultLocation}`);
   }
 
 document.addEventListener('DOMContentLoaded', () => {
-
   console.log('IronGenerator JS imported successfully!');
 }, false);
 
 window.addEventListener("load", () => {
-
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 13,
     center: defaultLocation,
   });
+
+  //Get parks
+  function getParks() {
+    axios
+      .get("home/api")
+      .then((response) => {
+        console.log(response);
+        placeParks(response.data.parks);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  //Create parks markers
+
+  function placeParks(parks) {
+    let markers = [];
+    for (let park of parks) {
+      const center = {
+        lat: park.location.coordinates[1],
+        lng: park.location.coordinates[0],
+      };
+      const pin = new google.maps.Marker({
+        position: center,
+        map: map,
+        title: park.name,
+      });
+      markers.push(pin);
+    }
+  }
+
+  getParks();
 });
 
