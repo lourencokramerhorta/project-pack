49f1f09a831b6480592bb82adbd323f9ac489b81
Lourenço Krämer Horta <lourencokramerhorta@gmail.com>
2021-02-20 15:39:40 +0000

    connection to mongodb cloud

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 //Connect to mongo
 mongoose
-  .connect("mongodb://localhost/project-node", { useNewUrlParser: true })
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@cluster0.ko1qs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  )
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
