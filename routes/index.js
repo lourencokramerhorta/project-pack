63db139857fbc683d8cf30be138a317386ded83a
catarinabombaca <42444764+catarinabombaca@users.noreply.github.com>
2021-02-20 15:24:43 +0000

    changed render of '/' route to home view

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,20 @@
 //Require express and create router
 const express = require('express');
 const router = express.Router();
+const Park = require("../models/Park.model");
 
 //GET home page
 router.get('/', (req, res, next) => {
-  const data = { notPartial: true };
-  res.render('index', data);
+  // const data = { notPartial: true };
+  // res.render('index', data);
+   Park.find()
+     .then((parks) => {
+       res.render("park/home", {
+         parks,
+         currentUser: req.session.currentUser,
+       });
+     })
+     .catch((err) => console.log(err));
 });
 
 module.exports = router;
