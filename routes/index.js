96affce07e755c370571c9a983df3eab8619cce6
catarinabombaca <catarinaipbombaca@gmail.com>
2021-05-29 18:17:26 +0100

    created env var for google maps api key

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 //Require express and create router
+require("dotenv").config()
 const express = require('express');
 const router = express.Router();
 const Park = require("../models/Park.model");
@@ -12,6 +13,7 @@ router.get('/', (req, res, next) => {
        res.render("park/home", {
          parks,
          currentUser: req.session.currentUser,
+         maps_url: `https://maps.googleapis.com/maps/api/js?key=${process.env.MAPS_API_KEY}`
        });
      })
      .catch((err) => console.log(err));
